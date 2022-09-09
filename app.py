aaa4564dc852582b82d991285496e194e3bc6d81
Gabriel.Arrien <garrien1@gmail.com>
2022-09-09 18:00:38 -0300

    bate ponto & access terminal

---
+++ app.py
@@ -0,0 +1,14 @@
+import pyautogui
+from time import sleep
+
+#bate ponto
+#pyautogui.click(24,453,duration=1)
+#pyautogui.click(950,664,duration=1)
+#pyautogui.click(675,47,duration=1)
+#pyautogui.click(986,476,duration=1)
+
+#acessar terminal
+pyautogui.click(31,1049,duration=1)
+pyautogui.click(967,65,duration=1)
+pyautogui.write('terminal')
+pyautogui.click(965,169,duration=1)
